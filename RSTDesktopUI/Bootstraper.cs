4b10317665db5d09ca9c604dcd745e9c19ee7bc7
Rafał Stępień <r.stepien@kan.net.pl>
2019-11-21 21:07:25 +0100

    DI with Caliburnmicro implemented

--- RSTDesktopUI/Bootstraper.cs
+++ RSTDesktopUI/Bootstraper.cs
@@ -11,14 +11,48 @@ namespace RSTDesktopUI
 {
     public class Bootstraper: BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstraper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(x => x.IsClass)
+                .Where(x => x.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
         }
+
+
     }
 }
