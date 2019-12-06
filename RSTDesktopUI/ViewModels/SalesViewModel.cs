342be1d1fd8fa646593158320ca11f3445dfa557
Rafał Stępień <r.stepien@kan.net.pl>
2019-12-06 21:57:41 +0100

    Simplyfi Tax calculation

--- RSTDesktopUI/ViewModels/SalesViewModel.cs
+++ RSTDesktopUI/ViewModels/SalesViewModel.cs
@@ -107,13 +107,17 @@ namespace RSTDesktopUI.ViewModels
         {
             decimal taxAmount = 0;
             decimal taxRate = _configHelper.GetTaxRate();
-            foreach (var item in Cart)
-            {
-                if (item.Product.IsTaxable)
-                {
-                    taxAmount += item.Product.RetailPrice * item.QuantityInCart * (taxRate / 100);
-                }
-            }
+            taxAmount = Cart.
+                Where(x => x.Product.IsTaxable)
+                .Sum(x => x.Product.RetailPrice * x.QuantityInCart * taxRate);
+
+            //foreach (var item in Cart)
+            //{
+            //    if (item.Product.IsTaxable)
+            //    {
+            //        taxAmount += item.Product.RetailPrice * item.QuantityInCart * (taxRate / 100);
+            //    }
+            //}
             return taxAmount;
         }
         public string Total 
